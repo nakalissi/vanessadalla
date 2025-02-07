5a5044665e4acefa04490d33464278b3c6cb3c96
nakalissi <65247079+nakalissi@users.noreply.github.com>
2025-02-07 18:39:31 +0000

    Deploying to main from @ nakalissi/vanessadalla@853c817977a02636c41da848be477b9599123454 🚀

--- src/components/Main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Main({ id, children }) {
-  return <main id={id}>{children}</main>;
-}
-
-export default Main;
--- src/components/Section.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function Section({ children }) {
-  return <section className="d-flex align-items-center">{children}</section>;
-}
-
-export default Section;
